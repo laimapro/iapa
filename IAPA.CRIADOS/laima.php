e4d9e9b0a4029e0edf016fc4091b0493ee332202
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-07-03 21:55:23 -0300

    transferencia de repositorio pessoal para o pro

---
+++ IAPA.CRIADOS/laima.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laima</title>
+</head>
+<body>
+    <audio controls>
+        <source src="IAPA/snd/assistente.mp3" type="audio/mpeg">
+        Seu navegador nÃ£o suporta o elemento de Ã¡udio.
+    </audio>
+</body>
+</html>
