67e58736a999b3b473a7a63ddcf44acd277652e2
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-07-06 17:19:07 -0300

    atualização de texto

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
-<footer class="text-center footer pt-5">Laima é um projeto desenvolvido na <a href="https://www.ufpe.br" target="_blank" title="o site da UFPE irá abrir uma nova aba">Universidade Federal de Pernambuco (UFPE)</a>.</footer>
-
-
-
-<script src="js/tempo-horario.js"></script>
-</body>
+        <footer class="text-center footer py-5">
+           <p> IAPA é um produto do Laima (<span aria-label="Laboratory of Artificial Intelligence and Machine AID" lang="en-us">Laboratory of Artificial Intelligence and Machine AID</span>). <br>
+            Laima é um projeto da <a href="https://www.ufpe.br" target="_blank" title="o site da UFPE irá abrir uma nova aba">Universidade Federal de Pernambuco</a> (UFPE).</p>
+        </footer>
+        <script src="js/tempo-horario.js"></script>
+    </body>
 </html>
