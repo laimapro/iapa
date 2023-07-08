e9b377cb2c68f5a0801a3a934efd34c6669292c0
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-07-08 10:01:41 -0300

    mudança semántica da hierarquia da página

--- includes/logo.php
+++ includes/logo.php
@@ -5,4 +5,4 @@
     <img src="img/icone.svg" class="logo img-fluid bi me-2" width="100px" alt="logotipo do Laima">
     <span class="fs-4 fw-bold">IAPA</span>
 </a>
-<p class="my-3 fs-6 text-muted">Instrumento de Avaliação de Produção Acadêmica</p>
+<h1 class="my-3 fs-6 fw-normal text-muted">Instrumento de Avaliação de Produção Acadêmica</h1>
