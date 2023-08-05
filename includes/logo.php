d3fd9710e62e420f8c5c8438050ce27e47193c42
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-08-05 12:33:32 -0300

    mudança de logo

--- includes/logo.php
+++ includes/logo.php
@@ -1,10 +1,10 @@
 <div class="pb-5 text-center">
     <a href="index.php" class="link-body-emphasis text-decoration-none">
-        <img src="img/LOGO_IAPA.svg" width="120px" alt="Logotipo do IAPA" lang="pt-br">
-        <h1 class="my-3 fw-bold">IAPA</h1>
+        <img src="img/logo-novo.svg" height="120px" alt="Logotipo do IAPA" class="mb-" lang="pt-br">
+        <!-- <h1 class="my-3 fw-bold">IAPA</h1> -->
     </a>
-    <p class="fs-6 text-muted">
-        <span aria-label="Instrumento de Avaliação de Produção Acadêmica" lang="pt-br">Instrumento de Avaliação de Produção Acadêmica</span>
-        <!-- da Universidade Federal de Pernambuco (UFPE) -->
-    </p>
+    <p>
+    <h1 class="my-3 fw-light fs-4 text-muted" aria-label="Instrumento de Avaliação de Produção Acadêmica" lang="pt-br">
+        Instrumento de Avaliação de Produção Acadêmica
+    </h1>
 </div>
