301b20f1bd5bfedd1083986b12a669c4ed1c08a7
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-08-06 12:21:42 -0300

    substituição da url da logo

--- includes/logo.php
+++ includes/logo.php
@@ -1,6 +1,6 @@
 <div class="pb-5 text-center">
     <a href="index.php" class="link-body-emphasis text-decoration-none">
-        <img src="img/logo-novo.svg" height="120px" alt="Logotipo do IAPA" class="mb-" lang="pt-br">
+        <img src="img/logo.svg" height="120px" alt="Logotipo do IAPA" class="mb-" lang="pt-br">
         <!-- <h1 class="my-3 fw-bold">IAPA</h1> -->
     </a>
     <p>
