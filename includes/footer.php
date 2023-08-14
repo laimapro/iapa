bedf0db589271a708a216cd08cf28e2d4c1250be
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-08-14 16:42:30 -0300

    organização das logos e afins

--- includes/footer.php
+++ includes/footer.php
@@ -6,8 +6,8 @@
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/anchor-js/5.0.0/anchor.min.js" integrity="sha512-byAcNWVEzFfu+tZItctr+WIMUJvpzT2kokkqcBq+VsrM3OrC5Aj9E2gh+hHpU0XNA3wDmX4sDbV5/nkhvTrj4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-        <script src="js/main.js"></script>
         <script src="js/saudacao.js"></script>
+        <script src="js/main.js"></script>
 
     </body>
 </html>
