34e1a4c678aee8c796623895b291db2eaa80690e
Wagner Beethoven <wagnerbeethoven@gmail.com>
2023-09-13 18:52:50 -0300

    formatação do pdf

--- avaliado.php
+++ avaliado.php
@@ -39,24 +39,22 @@ ob_start();
       color: #777;
       font-size: .825rem;
       text-align: center !important;
-      margin-bottom: 1rem;
+      margin-bottom: .5rem;
     }
 
     a[href^=http]:after {
       content: " <" attr(href) ">";
     }
 
-    .pdf-grade,
-    .pdf-item {
-      padding: 0 .5rem;
-    }
+
 
     ol li:nth-child(even) {
       background-color: #f2f2f2;
     }
 
     .pdf-grade {
-      text-align: center;
+      text-align: right;
+
     }
 
     .pdf-grade-final {}
@@ -66,27 +64,20 @@ ob_start();
   // Defina os estilos
   $styles = "
 <style>
-*{margin: 0;padding: 0;}
-body {font-size: 14px;font-family: sans-serif;}
-.iapa-info {color: #777;font-size: .825rem;margin-bottom: 1rem;text-align: center;}
+*{margin: 2pt;padding: 0;}
+body {font-size: 14px;font-family: Arial, sans-serif !important;}
+.iapa-info {color: #777;font-size: .825rem;margin-bottom: .5rem;text-align: center;}
 table {width: 100%;}
-.iapa-header {margin-bottom: 1rem;border-collapse: collapse;}
-.iapa-header-subtable {margin: 0.5rem 0 1rem;font-size: .85rem;color: #333;}
-.iapa-header-logo {padding-right: 1rem;}
+.iapa-header {margin-bottom: .5rem;border-collapse: collapse;}
+.iapa-header-subtable {margin: 0.5rem 0 .5rem;font-size: .85rem;color: #333;}
+.iapa-header-logo {padding-right: .5rem;}
 .iapa-title-article {padding-bottom: 0.5rem;font-size: 22px;text-transform: uppercase;font-weight: border;border-bottom: 1px solid #ddd;}
-.content-intro p {margin-bottom: 1rem;}
-.section-grade-title {font-size: 18px;margin: 2rem 0 .5rem;font-weight: 500;line-height: 1.2;}
-
-.section-grade-list {list-style: none;padding: 0;margin: 0;list-style-type: none;counter-reset: section;}
-.section-grade-list li:before{content: counters(section, ".") ". ";counter-increment: section;float: left;}     
-.section-grade-list li{padding: 0.5rem;overflow: auto;}
-.section-grade-list li:nth-child(even) {background-color: #f2f2f2;}
-.section-grade-list li div {float: left;}
-.section-grade-list li div:first-of-type {width: 75%;}
-.section-grade-list li div:last-of-type {width: 20%;text-align: right;}
-.section-grade-final {overflow: auto;display: inline-block;}
-.section-grade-final span {display: block;margin-bottom: 1.5rem;}
-.section-grade-final strong {text-align: center;border: 1px solid #ddd;border-radius: .825rem;font-weight: bold;font-size: 2rem;overflow: auto;display: block;padding: 1rem;}
+.content-intro p {margin-bottom: .5rem;}
+.section-grade-title {font-size: 18px;margin: 2rem 0 .5rem;font-weight: bold;font-family: Arial, sans-serif !important}
+.section-grade-list tr td{padding: 0.25rem;}
+.section-grade-list tr:nth-child(even) {background-color: #f2f2f2;}
+.section-grade-final {border-radius: 0.5rem;padding: 1rem;border: 1px solid #ddd;border-radius: .825rem;}
+.section-grade-final strong {display: block;width: 100%;font-size: 2rem;}
 .alert{border-radius: 0.5rem;padding: 1rem;}
 .alert svg {margin-right: 0.5rem;width: 32px;height: 32px;}
 .alert strong{display: block;width: 100%;font-size: 2rem;}
@@ -152,26 +143,29 @@ table {width: 100%;}
   </div>";
 
 
-        echo '<h1 class="iapa-title-article">' . $titulo . '</h1>';
-        echo '<table class="iapa-header-subtable">';
-        echo '<tr>';
-        echo '<td width="33.5%" class="col instituicao">' . $instituicao . '</td>';
-        echo '<td width="33.5%" class="col curso">' . $curso . '</td>';
-        echo '<td width="33.5%" class="col centro">' . $programaposgraduacao . '</td>';
-        echo '</tr>';
-        echo '</table>';
-        echo '<section class="content">';
-        echo '<div class="content-intro">';
-        echo '<p class="title-avaliacao-secao"x>Tendo lido e avaliado a produção acadêmica, de acordo com os quesitos formais pré-estabelecidos, apresento meu parecer nestes termos, o trabalho avaliado.</p>';
-        echo '</div>';
+      echo '<h1 class="iapa-title-article">' . $titulo . '</h1>';
+      echo '<table class="iapa-header-subtable">';
+      echo '<tr>';
+      echo '<td width="33.5%" class="col instituicao">' . $instituicao . '</td>';
+      echo '<td width="33.5%" class="col curso">' . $curso . '</td>';
+      echo '<td width="33.5%" class="col centro">' . $programaposgraduacao . '</td>';
+      echo '</tr>';
+      echo '</table>';
+      echo '<section class="content">';
+      echo '<div class="content-intro">';
+      echo '<p class="title-avaliacao-secao"x>Tendo lido e avaliado a produção acadêmica, de acordo com os quesitos formais pré-estabelecidos, apresento meu parecer nestes termos, o trabalho avaliado.</p>';
+      echo '</div>';
 
 
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        echo '<table class="section-grade-list" style="width: 100%; border-collapse:collapse">';
         if (isset($_POST["apresenta"])) {
           $numeracao = 1;
 
           foreach ($_POST["apresenta"] as $checkbox) {
-            echo "<li class='px-0 list-group-item d-flex justify-content-between align-items-start'>" . $numeracao . ". " . $checkbox . "</li>";
+            echo '<tr>';
+            echo '<td width="85%" class="col ms-2 pdf-item flex-fill">' . $numeracao . ". " . $checkbox . "</td>";
+            echo '</tr>';
             $numeracao++;
 
             if (isset($_POST[$checkbox])) {
@@ -179,16 +173,15 @@ table {width: 100%;}
             } else {
               // nothing
             }
-
-            echo "<br>";
           }
         } else {
           echo "";
         }
+        echo '</table>';
       }
 
       echo "<p class='title-avaliacao-secao'>Considerando os aspectos formais exigidos para a aprovação deste trabalho, julguei os quesitos que abaixo identifico e a eles aferi as seguintes notas:</p><br>";
-      echo '<ol class="list-group list-group-flush list-group-numbered">';
+      echo '<table class="section-grade-list" style="width: 100%; border-collapse:collapse">';
       if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (isset($_POST["label"]) && isset($_POST["nota"])) {
           $labels = $_POST["label"]; // Armazena os textos das labels em uma variável
@@ -201,50 +194,59 @@ table {width: 100%;}
             $valorInt = intval($valor); // Converte o valor para inteiro
 
             if ($valorInt !== 0) { // Verifica se o valor é diferente de zero
-              echo '<li  class="px-0 list-group-item d-flex justify-content-between align-items-start"><div class="col ms-2 pdf-item flex-fill">' . $labels[$index] . '</div>';
-              echo "<div class='col-1 pdf-grade'>Nota: " . $valorInt . "</div></li>";
+              echo '<tr>';
+              echo '<td width="85%" class="col ms-2 pdf-item flex-fill">' . $labels[$index] . '</td>';
+              echo "<td  width='15%' class='col-1 pdf-grade'>Nota: <strong>" . $valorInt . '</strong></td>';
+              echo "</tr>";
               $soma += $valorInt; // Acumula os valores para calcular a média
               $cont++; // Incrementa o contador de valores
             }
           }
-          echo '</ol>';
+          echo '</table>';
           if ($cont > 0) {
             $media = $soma / $cont;
             $mediaFormatada = number_format($media, 2); // Formata a média com duas casas decimais
-            echo '<h2 class="section-grade-title">Nota Final</h2>';
+            echo '<table>';
+            echo '<tr>';
+            echo '<td style="width: 35%"><h2 class="section-grade-title" style="font-family:Arial, sans-serif">Nota Final</h2>';
             echo '<div class="section-grade-final">';
-            echo '<span>Média da avaliação da produção acadêmica</span>';
+            echo '<span>Média da produção acadêmica</span>';
             echo '<strong>' . $mediaFormatada . '</strong>';
-            echo '</div>';
+            echo '</div></td>';
+            echo '<td style="width: 2.5%"></td>';
+            echo '<td style="width: 62.5%">';
 
-            echo '<h2 class="section-grade-title">Situação da produção acadêmica</h2>';
+            echo '<h2 class="section-grade-title" style="font-family:Arial, sans-serif">Situação da produção acadêmica</h2>';
 
             if ($mediaFormatada < 5.5) {
-                echo '<div class="alert alert-reprovado">Produção Acadêmica';
-                echo '<strong>';
-                echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/></svg>';
-                echo 'Reprovada';
-                echo '</strong>';
-                echo '</div>';
+              echo '<div class="alert alert-reprovado">Produção Acadêmica';
+              echo '<strong>';
+              echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/></svg>';
+              echo 'Reprovada';
+              echo '</strong>';
+              echo '</div>';
             } elseif ($mediaFormatada >= 5.5 && $mediaFormatada < 7) {
-                echo '<div class="alert alert-restricao">Produção Acadêmica';
-                echo '<strong>';
-                echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/></svg>';
-                echo 'Aprovada com restrições';
-                echo 'Veja observações do avaliador(a).';
-                echo '</strong>';
-                echo '</div>';
+              echo '<div class="alert alert-restricao">Produção Acadêmica';
+              echo '<strong>';
+              echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16"><path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/></svg>';
+              echo 'Aprovada com restrições';
+              echo 'Veja observações do avaliador(a).';
+              echo '</strong>';
+              echo '</div>';
             } elseif ($mediaFormatada >= 7) {
-                echo '<div class="alert alert-sucesso">Produção Acadêmica';
-                echo '<strong>';
-                echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16"><path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/></svg>';
-                echo 'Aprovada';
-                echo '</strong>';
-                echo '</div>';
+              echo '<div class="alert alert-sucesso">Produção Acadêmica';
+              echo '<strong>';
+              echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trophy-fill" viewBox="0 0 16 16"><path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5zm.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935z"/></svg>';
+              echo 'Aprovada';
+              echo '</strong>';
+              echo '</div>';
             }
           } else {
             echo "Nenhum valor selecionado ou todos os valores são zero.";
           }
+          echo '</td>';
+          echo '</tr>';
+          echo '</table>';
         } else {
           echo "Nenhum valor selecionado.";
         }
